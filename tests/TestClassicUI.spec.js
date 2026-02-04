2b5b5e42cfcb2febd173e708dcd93be37d6d5a12
Haikal <mhhaikal.333@gmail.com>
2026-02-04 18:32:21 +0700

    update 04/02/26

--- tests/TestClassicUI.spec.js
+++ tests/TestClassicUI.spec.js
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test';
-import  {playerSongTitles, 
+import  {
+  playerSongTitles, 
+  playerOffButton,
   datePickerCalendarContainer, 
   settingsDialog, 
   ButtonOK, 
@@ -9,7 +11,7 @@ import  {playerSongTitles,
   settingsButton, 
   settingsSaveButton}  from './selectors.js';
 
-const URL = 'https://live-beta.radio.cloud/?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImp0aSI6IjhhNzliODI3LTI1Y2QtNDc4Mi05MDcxLTc0MGIxYWZjNzJmYSJ9.eyJ1c2VyTGl2ZUlkIjoiYjMzNGIyOWUtNmMwZi00ZTNhLWEzNDEtMWJjMDQ4MDAwY2JlIiwicm9vbUlEIjoiRlJCLUIxLUJXRSIsImNhbktpY2tVc2VyIjpmYWxzZSwibWljRmFkZXJJZCI6NiwibWljRmFkZXJMYWJlbCI6IlRFU1QiLCJiaXRSYXRlIjoyNTYwMDAsIm1pY0Fsd2F5c09uIjpmYWxzZSwic2VydmVyQ29kZSI6IkFXUy1JRC1KS1QiLCJuZXR3b3JrQ29kZSI6IkJXRSIsImFsbG93RmFkZXJDaGFuZ2UiOnRydWUsImlzU2VydmVyIjowLCJlY2hvQ29tcGVuc2F0aW9uRW5hYmxlIjpmYWxzZSwicmVvcmRlckZhZGVyIjpmYWxzZSwidGltZVpvbmUiOiJFdXJvcGUvQmVybGluIiwibmV0d29ya01vZGUiOmZhbHNlLCJhbHRlcm5hdGVWaWV3Ijp0cnVlLCJjb25uZWN0QnJpZGdlIjpmYWxzZSwicmFkaW9JbmZvcm1hdGlvbiI6W3sicmFkaW9OYW1lIjoiQmF5ZXJud2VsbGUiLCJyYWRpb0ltYWdlIjoiaHR0cHM6Ly9kYjFxdTZzd3lxZjZ4LmNsb3VkZnJvbnQubmV0L2FmZmlsaWF0ZXMvYndlLnN2ZyIsInJhZGlvQ29kZSI6IkZSQiIsImNoYW5uZWxJZCI6IjU2MyIsIm5ldHdvcmtJZCI6IjYwIiwiZm9ybWF0SWQiOiIxIn1dLCJsb2NhbENvZGUiOiJGUkIiLCJpc19zaGlmdF9jaGVjayI6ZmFsc2UsImp0aSI6IjhjNzAwNGVmLWU3MTktNGNlMS1iM2JkLWZhZmY4MGVlYjg2ZCIsImlhdCI6MTc2OTY3MDgzMCwiZXhwIjoxODIxNTEwODMwfQ.HTwl3vwJ1Ti-MGc_OtCalDlO2P6NZbCIG6Z54aI_fIFw_6BkfSPYLgvWMgDUtASA9VFQBFEtWcaNPffFEocdgJnBQ_s45qkUR1M4mKP0Qx9x9DLpay6pRc7sA-XnKmmFQR9I305--kCQnmlkZMUMh549kmx7fAbrMDyBq4X113mOdxCMvkfXChem0OQXDKfWjaLaXHL2cgPGD6QgPOM_BWROKtJCbtN8y_z9Lf6n1PcssyRlwuMXwY8fgcMd91f_Drarp5b4MgWxByDrMcw9Tk4ptxeaQD6nu6gjjL4Agb1GTkY4w_fxh3fX4TPSJRZjtvajvfB8w0MFOt-ax39bQoK2bzGH1TH6sFqZfPPf715E-BBTTdBnG0jWq9SoSDcOKb7IY1x-TKXwWfFEN6PGcrrbCgyLQzssUMdPPSZQqXsEDBQGGh4E_dR0kyxfc9SrgTiusRoEPjLJGALrIsDUzcPC9GVV4wGuPE0J8qWVUyqOJsKqJ8vJYofFzYoRUzfuQ5kJaTsjp84naH_kDcc4OA2dKdqDcSUY5zy9IWlPCBc0ZqX83s2D5QMyfC7dixR10-yhTdXmLkq-CHXBl0egKZHF0B-RMDZLWFZF-m1lVbbF6yTw2fg-W9Lk0bH7xk1LEvT4ywWoO9OMTBr6OtW5crErDGaQ7EjBfnAxxyBLoFc';
+const URL = 'https://live-beta.radio.cloud/?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImp0aSI6IjM1MTAxOGFiLTg5NzgtNDZlMS05ODA4LWQyZWNhNjJjZTM4OCJ9.eyJ1c2VyTGl2ZUlkIjoiYjMzNGIyOWUtNmMwZi00ZTNhLWEzNDEtMWJjMDQ4MDAwY2JlIiwicm9vbUlEIjoiRlJCLUIxLUJXRSIsImNhbktpY2tVc2VyIjpmYWxzZSwibWljRmFkZXJJZCI6NiwibWljRmFkZXJMYWJlbCI6IlRFU1QiLCJiaXRSYXRlIjoyNTYwMDAsIm1pY0Fsd2F5c09uIjpmYWxzZSwic2VydmVyQ29kZSI6IkFXUy1JRC1KS1QiLCJuZXR3b3JrQ29kZSI6IkJXRSIsImFsbG93RmFkZXJDaGFuZ2UiOnRydWUsImlzU2VydmVyIjowLCJlY2hvQ29tcGVuc2F0aW9uRW5hYmxlIjpmYWxzZSwicmVvcmRlckZhZGVyIjpmYWxzZSwidGltZVpvbmUiOiJFdXJvcGUvQmVybGluIiwibmV0d29ya01vZGUiOmZhbHNlLCJhbHRlcm5hdGVWaWV3IjpmYWxzZSwiY29ubmVjdEJyaWRnZSI6ZmFsc2UsInJhZGlvSW5mb3JtYXRpb24iOlt7InJhZGlvTmFtZSI6IkJheWVybndlbGxlIiwicmFkaW9JbWFnZSI6Imh0dHBzOi8vZGIxcXU2c3d5cWY2eC5jbG91ZGZyb250Lm5ldC9hZmZpbGlhdGVzL2J3ZS5zdmciLCJyYWRpb0NvZGUiOiJGUkIiLCJjaGFubmVsSWQiOiI1NjMiLCJuZXR3b3JrSWQiOiI2MCIsImZvcm1hdElkIjoiMSJ9XSwibG9jYWxDb2RlIjoiRlJCIiwiaXNfc2hpZnRfY2hlY2siOmZhbHNlLCJqdGkiOiJmNjk3NGI4NC1iZTc0LTQxZDEtOWRkOS01NDk1MGNiODEwMjMiLCJpYXQiOjE3NzAxODc2MDUsImV4cCI6MTgyMjAyNzYwNX0.HspHp-u3yGdFEQtV6MpbejHwvu5IzxXdPembeewW38aurdnfL_NuFA-C9D2iJu_9JCLlRPc6PqW2Dzd-a_925qUq8vT8QdOe1GzvNbJ4ueDxFvEQs5seIgNgldq9LmoYPIk_RB09-yWqZ36SAHO8ntLn0dAxoCaAZ913-fU8_oQHTKa926I0xgx8AoV1UXhIsUiiJtS-4gqgkHzmwBC3W4f1WRx0q2WNPeTVIQeuJ3W-pK4al-v-wyLIEYgXe4bBFczXwJgSso6n_TanndVJ0WVZyIhqg_YJCo8sW6oBPjxafkNZd64W6AJpfBH4csvaIOkuoLC0Cc7-H4jcxvHIa5d8kbjl3UE0vxt_SFqWSwHFUdTtycxCFUmuvn8-jY-UeZYXYnBCbIbNssBegqwJnqcgiMaft0OyVv5KMziBk1sHDwX78XrHg7fg6PfiXnCCzOEeQH3__WceFHyW3Lx9Yp0Ajw4IcwP3j9MI4Pc5yL9KTJSjJ5-U8X6f-iO_1c-R2dsv2_kH3ENXWwtFwD0TsiVGyT3pCoKi4KmjgWdpDTti95tpetEy313SPVZ12eE0GpKM9DOKduC697ebYGcFs9szs6PxMeheb3saHUNg_95GvNd-n8jDZ-RiH4xxyD0aNghwAzAEL_kFPFT2zIwRS7Sn8r37MsbONJrsX1heoT0';
 
 
 test.beforeEach(async ({ page }) => {
@@ -33,7 +35,7 @@ test.beforeEach(async ({ page }) => {
   await expect(settingsBtn).toBeEnabled();
   await settingsBtn.click();
 
-  const overlaysetting = page.getByText('Audio Replace ModeTrueFalseDisable Keyboard ShortcutsBoost ModeEcho');
+  const overlaysetting = page.locator('.setting-dlg');
   await expect(overlaysetting).toBeVisible();
   await expect(overlaysetting).toBeEnabled();
 
@@ -44,11 +46,6 @@ await AudioReplaceMode.click();
 await AudioReplaceMode.selectOption('a');
 await expect(AudioReplaceMode).toHaveValue('a');
 
-const DisableKeyboardShortcuts = page.locator('.react-switch-bg').first();
-await expect(DisableKeyboardShortcuts).toBeVisible();
-await expect(DisableKeyboardShortcuts).toBeEnabled();
-await DisableKeyboardShortcuts.click();
-
 const Save = page.locator(settingsSaveButton());
 await expect(Save).toBeVisible();
 await expect(Save).toBeEnabled();
@@ -57,7 +54,7 @@ await Save.click();
 await page.waitForTimeout(6000);
   await page.keyboard.press('Escape');
   
-const Setting = page.locator(settingsDialog());
+const Setting = page.locator('.setting-dlg');
 await expect (Setting).not.toBeVisible();
 await page.waitForTimeout(6000);
 
@@ -173,54 +170,60 @@ await page.waitForTimeout(16000);
 
 test('Link Mode Check', async ({ page }) =>  {
 
-const Content1Linkmode = page.locator(playlistItemByIndex()).nth(8);
+  await page.waitForTimeout(5000);
+
+const Content1Linkmode = page.locator(playlistItemByIndex()).nth(12);
 await Content1Linkmode.scrollIntoViewIfNeeded();
 await expect(Content1Linkmode).toBeVisible();
 await expect(Content1Linkmode).toBeEnabled({ timeout: 10000 }); 
 
-const Linkmode1 = page.locator(playlistItemLinkModeIcon()).nth(8);
+const Linkmode1 = page.locator(playlistItemLinkModeIcon()).nth(12);
 await expect(Linkmode1).toBeVisible();
 await expect(Linkmode1).toBeEnabled();
 await Linkmode1.click();
 console.log('Clicked on Linkmode icon of Content1Linkmode');
 await page.waitForTimeout(5000);
 
-const Content2Linkmode = page.locator(playlistItemByIndex()).nth(9);
+const Content2Linkmode = page.locator(playlistItemByIndex()).nth(13);
 await Content2Linkmode.scrollIntoViewIfNeeded();
 await expect(Content2Linkmode).toBeVisible();
 await expect(Content2Linkmode).toBeEnabled({ timeout: 10000 }); 
 
-const Linkmode2 = page.locator(playlistItemLinkModeIcon()).nth(9);
+const Linkmode2 = page.locator(playlistItemLinkModeIcon()).nth(13);
 await expect(Linkmode2).toBeVisible();
 await expect(Linkmode2).toBeEnabled();
 await Linkmode2.click();
 console.log('Clicked on Linkmode icon of Content2Linkmode');
 await page.waitForTimeout(5000);
 
-const Content3Linkmode = page.locator(playlistItemByIndex()).nth(10);
+const Content3Linkmode = page.locator(playlistItemByIndex()).nth(14);
 await Content3Linkmode.scrollIntoViewIfNeeded();
 await expect(Content3Linkmode).toBeVisible();
-await expect(Content3Linkmode).toBeEnabled({ timeout: 10000 });
+await expect(Content3Linkmode).toBeEnabled({ timeout: 10000 }); 
 
-const Linkmode3 = page.locator(playlistItemLinkModeIcon()).nth(10);
+const Linkmode3 = page.locator(playlistItemLinkModeIcon()).nth(14);
 await expect(Linkmode3).toBeVisible();
 await expect(Linkmode3).toBeEnabled();
 await Linkmode3.click();
 console.log('Clicked on Linkmode icon of Content3Linkmode');
 await page.waitForTimeout(5000);
 
-const player1linkmode= page.locator(playerByRole('player1'));
+const player1linkmode= page.locator(playerByRole('player1')); 
 await expect(player1linkmode).toBeVisible();
 await expect(player1linkmode).toBeEnabled();
 
-await Content1Linkmode.dragTo(player1linkmode);
+await Content1Linkmode.dragTo(player1linkmode); 
 await page.mouse.up();
 
 
-console.log('Drag and Drop Linkmode Content to Player1');
-await player1linkmode.click();
-await page.waitForTimeout(7000);
-await page.keyboard.press('1');
+console.log('Drag and Drop Linkmode Content to Player1'); 
+
+const player1button= page.locator(playerOffButton('1'));
+await expect(player1button).toBeVisible();
+await expect(player1button).toBeEnabled();
+await player1button.click();
+
+
 console.log('---------------------------player 1 link mode started--------------------------');
 
 
